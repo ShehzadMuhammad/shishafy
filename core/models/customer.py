5ff44e93477b850d2e56fa65cf9a954db0dc1e45
ShehzadMuhammad <shehzad.muhammad@outlook.com>
2025-02-06 16:02:50 -0500

    Create Interactor and Update Models

--- core/models/customer.py
+++ core/models/customer.py
@@ -1,20 +1,10 @@
-from django.core.validators import RegexValidator
-from django.db.models import CharField, Model
+from django.db.models import CharField, EmailField, Model
 
 
 class Customer(Model):
     name = CharField(max_length=65)
-    email = CharField(max_length=65)
-    phone_number = CharField(
-        max_length=10,
-        unique=True,
-        validators=[
-            RegexValidator(
-                regex=r"^\d{10}$",
-                message="Phone Number mustb be exactly 10 digits ie 4162341111",
-            )
-        ],
-    )
+    email = EmailField(unique=True)
+    phone_number = CharField(max_length=10, unique=True)
 
     def __str__(self):
         return f"{self.name} - {self.phone_number} - {self.email}"
